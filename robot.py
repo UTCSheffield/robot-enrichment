3dfad77fb4f9cd5bb740a856268fac57d4f55a03
Conor Hudson <mind.pixelated@gmail.com>
2015-03-12 16:39:30 +0000

    reversing 1.5 sec

--- robot.py
+++ robot.py
@@ -1,14 +1,19 @@
-import explorerhat, signal
+import explorerhat, signal, time
 
 #What to do when an IR sensor changes
 def handle_analog(pin, value):
   print (pin.name, value)
   if (pin.name == "one" and value > 2):
+    explorerhat.motor.one.backward()
+    explorerhat.motor.two.backward()
+    time.sleep(1.5)
+    
     explorerhat.motor.one.forward()
     explorerhat.motor.two.backward()
-  else:
+    time.sleep(1)
+    explorerhat.motor.one.forward()
     explorerhat.motor.two.forward()
-
+    
 #Start waiting for changes on the IR sensor
 explorerhat.analog.one.changed(handle_analog)
 #signal.pause()
