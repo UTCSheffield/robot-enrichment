e06929bd9731c19084f04c09af1770bcd730213e
Conor Hudson <mind.pixelated@gmail.com>
2015-03-19 16:41:11 +0000

    Update robotserver.py

--- python/robotserver.py
+++ python/robotserver.py
@@ -75,7 +75,7 @@ class RobotServer(object):
         
   
     def handle_analog(self, pin, value):
-        print (pin.name, value, self.status)
+        #print (pin.name, value, self.status)
         if (value > 2):
             if (self.status != "danger"):
                 self.oldstatus = self.status
