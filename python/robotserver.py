1cf1ef1d293d96df884c338c29fc51d80d37b7de
Mr Eggleton <meggleton@utcsheffield.org.uk>
2015-03-19 17:05:25 +0000

    don't let the sensors make it move from stopped  (it falls off tables)

--- python/robotserver.py
+++ python/robotserver.py
@@ -76,7 +76,7 @@ class RobotServer(object):
   
     def handle_analog(self, pin, value):
         #print (pin.name, value, self.status)
-        if (value > 2):
+        if (value > 2 and self.status != "stop"):
             if (self.status != "danger"):
                 self.oldstatus = self.status
             self.status = "danger"
