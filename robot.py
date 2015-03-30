f26e220e53f886e7a502219830cf7a1de9bcc673
stretchyboy <martyn.eggleton@gmail.com>
2015-03-30 22:20:02 +0000

    Attempt at getting encoders to straighten the forward travel

--- robot.py
+++ robot.py
@@ -13,44 +13,81 @@ class RobotServer(object):
         explorerhat.analog.one.changed(self.handle_analog)
         explorerhat.analog.two.changed(self.handle_analog)
 
+        self.speed_factors = {"one":1, "two":0.8}
+        
         self.encoders = {"one":0, "two":0}
+
+        self.speed_adjust = 0.05;
 
         explorerhat.input.one.on_changed(self.handle_encoder, 15)
         explorerhat.input.two.on_changed(self.handle_encoder, 15)
         
         self.do_command(initialcommand)
-        
+
+    def reset_encoders(self):
+        self.encoders = {"one":0, "two":0}
+
+    def tune_speed_factors(self):
+
+        if (self.status == "forward"):
+            ticks = (self.encoders["one"] + self.encoders["two"])
+            #if ticks >= 30 and (ticks % 10) == 0:
+            if (ticks % 8) == 0:
+                #both start at 100 and slow the faster one until they match. if the one that isn't 100 is now the slower one speed it up.
+                if self.encoders["one"] > self.encoders["two"]:
+                    if self.speed_factors["two"] < 1:
+                        self.speed_factors["two"] = min(self.speed_factors["two"] + self.speed_adjust, 1)
+                    else:
+                        self.speed_factors["one"] = max(self.speed_factors["one"] - self.speed_adjust, 0.5)
+                elif self.encoders["one"] < self.encoders["two"]:
+                    if self.speed_factors["one"] < 1:
+                        self.speed_factors["one"] = min(self.speed_factors["one"] + self.speed_adjust, 1)
+                    else:
+                        self.speed_factors["two"] = max(self.speed_factors["two"] - self.speed_adjust, 0.5)
+
+                print ("self.encoders", self.encoders, self.encoders["one"] - self.encoders["two"], "speed_factors", self.speed_factors)
+                explorerhat.motor.one.forward(self.speed * self.speed_factors["one"])
+                explorerhat.motor.two.forward(self.speed * self.speed_factors["two"])
 
     @cherrypy.expose
-    def forward(self, speed=95):
-        explorerhat.motor.one.forward(int(speed))
-        explorerhat.motor.two.forward(int(speed))
+    def forward(self, speed=100):
+        self.speed = int(speed)
+        self.reset_encoders()
+        explorerhat.motor.one.forward(self.speed * self.speed_factors["one"])
+        explorerhat.motor.two.forward(self.speed * self.speed_factors["two"])
         self.status = "forward"
         return self.status
     
     @cherrypy.expose
-    def backward(self, speed=95):
-        explorerhat.motor.one.backward(int(speed))
-        explorerhat.motor.two.backward(int(speed))
+    def backward(self, speed=100):
+        self.speed = int(speed)
+        self.reset_encoders()
+        explorerhat.motor.one.backward(self.speed * self.speed_factors["one"])
+        explorerhat.motor.two.backward(self.speed * self.speed_factors["two"])
         self.status = "backward"
         return self.status
         
     @cherrypy.expose
-    def right(self, speed=95):
-        explorerhat.motor.one.forward(int(speed))
-        explorerhat.motor.two.backward(int(speed))
+    def right(self, speed=100):
+        self.speed = int(speed)
+        self.reset_encoders()
+        explorerhat.motor.one.forward(self.speed * self.speed_factors["one"])
+        explorerhat.motor.two.backward(self.speed * self.speed_factors["two"])
         self.status = "right"
         return self.status
     
     @cherrypy.expose
-    def left(self, speed=95):
-        explorerhat.motor.one.backward(int(speed))
-        explorerhat.motor.two.forward(int(speed))
+    def left(self, speed=100):
+        self.speed = int(speed)
+        self.reset_encoders()
+        explorerhat.motor.one.backward(self.speed * self.speed_factors["one"])
+        explorerhat.motor.two.forward(self.speed * self.speed_factors["two"])
         self.status = "left"
         return self.status
     
     @cherrypy.expose
     def stop(self):
+        self.reset_encoders()
         explorerhat.motor.one.stop()
         explorerhat.motor.two.stop()
         self.status = "stop"
@@ -85,7 +122,9 @@ class RobotServer(object):
   
     def handle_encoder(self, pin):
         self.encoders[pin.name] += 1
-        print (pin.name, self.encoders[pin.name], pin.last, pin.read() )
+        #print (pin.name, self.encoders[pin.name], pin.last, pin.read() )
+        self.tune_speed_factors()
+        
 
     def handle_analog(self, pin, value):
         #print (pin.name, value, self.status)
@@ -93,6 +132,7 @@ class RobotServer(object):
             if (self.status != "bump"):
                 self.oldstatus = self.status
             self.status = "bump"
+            
             explorerhat.motor.one.stop()
             explorerhat.motor.two.stop()
             time.sleep(0.2)
@@ -112,7 +152,7 @@ class RobotServer(object):
             self.do_command(self.oldstatus)
             
 if __name__ == '__main__':
-    cherrypy.quickstart(RobotServer(), config="app.conf")
+    cherrypy.quickstart(RobotServer("forward"), config="app.conf")
     
     
     
