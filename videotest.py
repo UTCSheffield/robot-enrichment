cc963140052eb9639ce77cbf9a2c7b3b14f191cc
stretchyboy <martyn.eggleton@gmail.com>
2015-04-22 03:54:51 +0000

    make video stream longer

--- videotest.py
+++ videotest.py
@@ -5,8 +5,10 @@ import time
 import picamera
 
 with picamera.PiCamera() as camera:
-    camera.resolution = (640, 480)
-    camera.framerate = 24
+    #camera.resolution = (640, 480)
+    #camera.framerate = 24
+    camera.resolution = (320, 240)
+    camera.framerate = 15
 
     server_socket = socket.socket()
     server_socket.bind(('0.0.0.0', 8000))
@@ -16,8 +18,9 @@ with picamera.PiCamera() as camera:
     connection = server_socket.accept()[0].makefile('wb')
     try:
         camera.start_recording(connection, format='h264')
-        camera.wait_recording(60)
+        camera.wait_recording(6000)
         camera.stop_recording()
     finally:
+        #camera.stop_recording()
         connection.close()
         server_socket.close()
